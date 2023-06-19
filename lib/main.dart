ecffe729210252dcf84fd141580a02a2ebe12289
Prince Bioh <princebioh15@gmail.com>
2023-06-19 14:08:28 +0000

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:bubble_tea_ui/pages/sign_in_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SignInPage(),
+    );
+  }
+}
